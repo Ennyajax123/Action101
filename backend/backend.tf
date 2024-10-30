0685a1e4d2784d22015fc7c411444a9f70dd7010
Ennyajax123 <ajanaku.eniola123@gmail.com>
2024-10-30 22:00:42 +0000

    changes made on my scripts

--- backend/backend.tf
+++ backend/backend.tf
@@ -1,17 +1,18 @@
 # Creating a module to provision 2 Backend EC2 machines
 module "create-me backend1" {
-  source    = "../week6-proj-module/"
-  node_name = var.backend1_name
-  user_data  = file("./frontend-install.sh")
-  sg_name   = var.backend1_sg_name.id
-  ingress_port_1 = var.backend1_ingress_port_1
-  ingress_port_2 = var.backend1_ingress_port_2
+  source    = "../ACTION101-module/"
+  node_name = var.backend1
+  user_data  = file("./backend-install.sh")
+  vpc_id         = module.vpc_1.vpc_id
+  sg_name   = var.node_name_sg.id
+  ingress_port_1 = var.port_1
+  ingress_port_2 = var.port_2
 }
 module "create-me backend2" {
-  source    = "../week6-proj-module/"
-  node_name = var.backend2_name
-  user_data  = file("./frontend-install.sh")
-  sg_name   = var.backend2_sg_name.id
-  ingress_port_1 = var.backend2_ingress_port_1
-  ingress_port_2 = var.backend2_ingress_port_2
+  source    = "../ACTION101-module/"
+  node_name = var.backend2
+  user_data  = file("./backend-install.sh")
+  sg_name   = var.node_name_sg.id
+  ingress_port_1 = var.port_1
+  ingress_port_2 = var.port_2
 }
